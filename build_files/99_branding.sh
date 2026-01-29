3ee530a7f6e45af1263f3e82fc2b1ee5dd3a9e78
David Jeffreys <179866819+moorepastas@users.noreply.github.com>
2026-01-29 13:48:18 +0000

    change the release name

--- build_files/99_branding.sh
+++ build_files/99_branding.sh
@@ -2,4 +2,16 @@
 
 set -ouex pipefail
 
-#change etc/release
+#change release
+sed -i -f - /usr/lib/os-release <<EOF
+s|^NAME=.*|NAME=\"Nahobeno\"|
+s|^PRETTY_NAME=.*|PRETTY_NAME=\"Nahobeno\"|
+s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"Tokyo\"|
+s|^VARIANT_ID=.*|VARIANT_ID=""|
+s|^HOME_URL=.*|HOME_URL=""|
+s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=""|
+s|^SUPPORT_URL=.*|SUPPORT_URL=""|
+s|^CPE_NAME=\".*\"|CPE_NAME=\"cpe:/o:moorepastas:nahobeno\"|
+s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=""|
+s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME="nahobeno"|
+EOF
