6e1c64dddf84ebdabafe0b5b418751060b3b5546
David Jeffreys <179866819+moorepastas@users.noreply.github.com>
2026-02-02 16:27:26 +0000

    Update 99_branding.sh

--- build_files/99_branding.sh
+++ build_files/99_branding.sh
@@ -4,14 +4,14 @@ set -ouex pipefail
 
 #change release
 sed -i -f - /usr/lib/os-release <<EOF
-s|^NAME=.*|NAME=\"Nahobeno\"|
-s|^PRETTY_NAME=.*|PRETTY_NAME=\"Nahobeno\"|
-s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"Tokyo\"|
+s|^NAME=.*|NAME=\"Tree\"|
+s|^PRETTY_NAME=.*|PRETTY_NAME=\"Tree\"|
+s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"Oak\"|
 s|^VARIANT_ID=.*|VARIANT_ID=""|
 s|^HOME_URL=.*|HOME_URL=""|
 s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=""|
 s|^SUPPORT_URL=.*|SUPPORT_URL=""|
-s|^CPE_NAME=\".*\"|CPE_NAME=\"cpe:/o:moorepastas:nahobeno\"|
+s|^CPE_NAME=\".*\"|CPE_NAME=\"cpe:/o:moorepastas:tree\"|
 s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=""|
-s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME="nahobeno"|
+s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME="tree"|
 EOF
